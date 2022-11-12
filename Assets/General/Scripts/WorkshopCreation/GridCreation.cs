76a2078b193c69242063834b4f58ddbca6bf0813
coestephen2002 <66713914+coestephen2002@users.noreply.github.com>
2022-11-11 18:23:37 -0800

    Grid UI changes

--- Assets/General/Scripts/WorkshopCreation/GridCreation.cs
+++ Assets/General/Scripts/WorkshopCreation/GridCreation.cs
@@ -12,29 +12,30 @@ public class GridCreation : MonoBehaviour
     [SerializeField] Tile tilePrefab;
    public void GenerateGrid()
    {
-    height = heightSlide.value;
-    width = widthSlide.value;
-    Debug.Log(height);
-    Debug.Log(width);
-    for(int x = 0; x < width; x++) { 
-  
-        for(int y = 0; y<height; y++) { 
+        height = heightSlide.value;
+        width = widthSlide.value;
+        Debug.Log(height);
+        Debug.Log(width);
+        for(int x = 0; x < width; x++) { 
+            for(int y = 0; y<height; y++) { 
         
-            var spawnedTile = Instantiate(tilePrefab, new Vector3(x,y), Quaternion.identity);
-            spawnedTile.name = $"Tile ({x},{y})";
+                var spawnedTile = Instantiate(tilePrefab, new Vector3(x,y), Quaternion.identity);
+                spawnedTile.name = $"Tile ({x},{y})";
 
-            var isOffset = (x + y) % 2 == 1;
-             spawnedTile.init(isOffset);
+                var isOffset = (x + y) % 2 == 1;
+                spawnedTile.init(isOffset);
         }
     }
     
-    float maximum = Mathf.Max(width, height);
+        float maximum = Mathf.Max(width, height);
     
-    camera.orthographicSize = maximum/2.0f + maximum/100;
+        camera.orthographicSize = maximum/1.5f + maximum/100;
     
-    float cameraHeight = 2.0f * camera.orthographicSize;
-    float cameraWidth = cameraHeight * camera.aspect;
-    cam.transform.position = new Vector3(width/2.0f + cameraWidth/6.0f , height/2.0f - 0.5f, -10);
+        float cameraHeight = 2.0f * camera.orthographicSize;
+        float cameraWidth = cameraHeight * camera.aspect;
+        cam.transform.position = new Vector3(width/2.0f + cameraWidth/5.2f , height/2.0f - 0.5f, -10);
+        camera.backgroundColor = new Color(0.75f, 0.75f, 0.75f, 1.0f);
+     
 
    }
 }
