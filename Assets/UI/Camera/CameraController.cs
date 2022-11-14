5e5d0168d367872647bfef2619d5b02137a96dab
ImWesMan <66706588+ImWesMan@users.noreply.github.com>
2022-11-14 14:50:57 -0800

    Added zoom functionality only within boundary

--- Assets/UI/Camera/CameraController.cs
+++ Assets/UI/Camera/CameraController.cs
@@ -8,12 +8,33 @@ public class cameraController : MonoBehaviour
     private float zoomScale = 10.0f;
     private float zoomMin = 5.0f;
     private float zoomMax = 30.0f;
-
+    public GameObject topRight;
+    public GameObject topLeft;
+    public GameObject bottomRight;
+    public GameObject bottomLeft;
+     Vector3 topRightPos;
+     Vector3 topLeftPos;
+      Vector3 bottomRightPos;
+      Vector3 bottomLeftPos;
     // Update is called once per frame
-    
+     public void getPos() {
+        topRightPos = camera.WorldToScreenPoint(topRight.transform.position);
+        topLeftPos = camera.WorldToScreenPoint(topLeft.transform.position);
+        bottomRightPos = camera.WorldToScreenPoint(bottomRight.transform.position);
+        bottomLeftPos = camera.WorldToScreenPoint(bottomLeft.transform.position);
+        Debug.Log(topRightPos);
+        Debug.Log(topLeftPos);
+        Debug.Log(bottomRightPos);
+        Debug.Log(bottomLeftPos);
+    }
+
+
     void Update()
    {
+    if(Input.mousePosition.x > bottomLeftPos.x && Input.mousePosition.y > bottomLeftPos.y && Input.mousePosition.x < bottomRightPos.x && Input.mousePosition.y < topRightPos.y )
+    {
     Zoom(Input.GetAxis("Mouse ScrollWheel"));
+    }
    }
 
     private void Zoom(float zoomDiff)
