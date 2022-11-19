55f1c38272442181ee97faac12251d0660219247
ImWesMan <66706588+ImWesMan@users.noreply.github.com>
2022-11-18 21:27:48 -0800

    early item bank implementation
    this took 8 hours i am losing my mind

--- Assets/UI/ItemBank/toggleSizeChoice.cs
+++ Assets/UI/ItemBank/toggleSizeChoice.cs
@@ -2,13 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 public class toggleSizeChoice : MonoBehaviour
 {
     [SerializeField]
     GameObject gameObject;
+    [SerializeField]
+    TMP_Text statusText;
+    bool enabled;
+    GameObject[] buttonsBelow;
+
+   IEnumerator Start()
+   {
+    bool enabled = true;
+    setState();
+    yield return new WaitForSeconds(0.5f);
+    setState();
+   }
     // Start is called before the first frame update
    public void setState()
    {
+    enabled = !enabled;
     gameObject.SetActive(!gameObject.activeInHierarchy);
+    if(enabled == true)
+    {
+        buttonsBelow = GameObject.FindGameObjectsWithTag("ItemCategory");
+        foreach (GameObject button in buttonsBelow)
+        {
+            if(gameObject.transform.position.y > button.transform.position.y)
+            {
+                Debug.Log(Mathf.Ceil((gameObject.transform.childCount/3.0f)));
+                 RectTransform transform = button.GetComponent<RectTransform>();
+                 transform.anchoredPosition = new Vector2(transform.anchoredPosition.x, transform.anchoredPosition.y + Mathf.Ceil((gameObject.transform.childCount/3.0f) * 140));
+            }
+            
+        }
+        statusText.text = "+";
+    }
+    if(enabled == false)
+    {
+        buttonsBelow = GameObject.FindGameObjectsWithTag("ItemCategory");
+        foreach (GameObject button in buttonsBelow)
+        {
+            if(gameObject.transform.position.y > button.transform.position.y)
+            {
+                Debug.Log(Mathf.Ceil((gameObject.transform.childCount/3.0f)));
+                 RectTransform transform = button.GetComponent<RectTransform>();
+                 transform.anchoredPosition = new Vector2(transform.anchoredPosition.x, transform.anchoredPosition.y - Mathf.Ceil((gameObject.transform.childCount/3.0f) * 135));
+            }
+            
+        }
+         statusText.text = "-";
+    }
    }
 }
