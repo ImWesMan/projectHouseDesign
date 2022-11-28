9bf4fa95eee363a16c8aede594472da7e8300e34
Vera <vkorchemnaya@eagles.ewu.edu>
2022-11-27 19:24:38 -0800

    Working on Script

--- Assets/AddCustomItem.cs
+++ Assets/AddCustomItem.cs
@@ -5,9 +5,19 @@ using UnityEngine.UI;
 
 public class AddCustomItem : MonoBehaviour
 {
-    [SerializeField] Button button;
+    [SerializeField] GameObject FurnitureBank;
+    [SerializeField] GameObject WallBank;
+    
 
     public void addItem() {
+        if(FurnitureBank.activeSelf){
+            Debug.Log("Hello: " + FurnitureBank.name);
+            Button example = FurnitureBank.GetComponentInChildren<Button>();
+            Debug.Log("Hello: " + example.name);
+        }else if (WallBank.activeSelf){
+            Debug.Log("Hello: " + WallBank.name);
+        }
+        
         
     }
 }
