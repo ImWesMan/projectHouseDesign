60c0a16ff220b7e754cbfe116b6ca322ef6f4c25
Vera <vkorchemnaya@eagles.ewu.edu>
2022-12-09 19:22:22 -0800

    Create Custom
    Adds a newly-created custom item to custom item bank

--- Assets/AddCustomItem.cs
+++ Assets/AddCustomItem.cs
@@ -2,22 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class AddCustomItem : MonoBehaviour
 {
+    
     [SerializeField] GameObject FurnitureBank;
     [SerializeField] GameObject WallBank;
+    [SerializeField] TMP_Text label;
+    [SerializeField] TMP_Text width;
+    [SerializeField] TMP_Text length;
+    [SerializeField] TMP_InputField inputLabel;
+    [SerializeField] TMP_InputField inputWidth;
+    [SerializeField] TMP_InputField inputLength;
     
-
     public void addItem() {
+
         if(FurnitureBank.activeSelf){
-            Debug.Log("Hello: " + FurnitureBank.name);
-            Button example = FurnitureBank.GetComponentInChildren<Button>();
-            Debug.Log("Hello: " + example.name);
+            create(FurnitureBank);
         }else if (WallBank.activeSelf){
-            Debug.Log("Hello: " + WallBank.name);
+            create(WallBank);
         }
         
-        
+        clearFields();
+
     }
+
+    public void create(GameObject bank) {
+        
+        Transform container = RecursiveFindChild (bank.transform, "Custom Panel");
+        GameObject copy = container.Find("PlaceHolder").gameObject;
+
+        GameObject newObject = Instantiate(copy, container);
+        newObject.transform.Find("Label").gameObject.GetComponentInChildren<TMP_Text>().SetText(label.text);
+
+        string dimensions = "(" + width.text + "x" + length.text + ")";
+        newObject.transform.Find("Dimensions").gameObject.GetComponentInChildren<TMP_Text>().SetText(dimensions);
+        newObject.SetActive(true);
+
+    }
+
+    public void clearFields() {
+        inputLabel.text = null;
+        inputWidth.text = null;
+        inputLength.text = null;
+    }
+
+    Transform RecursiveFindChild(Transform parent, string childName)
+    {
+        foreach (Transform child in parent)
+        {
+            if(child.name == childName)
+            {
+                return child;
+            }
+            else
+            {
+                Transform found = RecursiveFindChild(child, childName);
+                if (found != null)
+                {
+                        return found;
+                }
+            }
+        }
+        return null;
+    }
+
 }
+
