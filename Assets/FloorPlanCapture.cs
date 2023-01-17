459eaa7584d8b95ad86fb8832fdaba3e3d53d387
Vera <vkorchemnaya@eagles.ewu.edu>
2023-01-17 13:13:07 -0800

    Capture Screen
    Added Export Image button and created the CaptureFloorPlan script that exports an image of the grid.

---
+++ Assets/FloorPlanCapture.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloorPlanCapture : MonoBehaviour
+{
+    public Canvas canvas;
+    public Transform cam;
+    private float width;
+    private float height;
+
+
+    public void ExportImage()
+    {
+        GridCreation gridScript = FindObjectOfType<GridCreation>();
+        width = gridScript.width;
+        height = gridScript.height;
+        gridScript.resetGrid();
+
+        RecenterCamera();
+
+        canvas.gameObject.SetActive(false);
+        ScreenCapture.CaptureScreenshot("Floorplan.png");
+        Invoke("Activate", 0.02f);
+
+    }
+
+    private void Activate(){
+        canvas.gameObject.SetActive(true);
+    }
+
+    public void RecenterCamera()
+    {
+        Vector3 gridCenter = new Vector3(width / 2.0f, height / 2.0f, 0);
+        cam.position = gridCenter;
+    }
+}
