76dc924d49b81482f748e2629de305dae58c5732
WesleyJak <jwesley@ewu.edu>
2023-01-30 14:51:41 -0800

    starting grid structure changes

--- Assets/General/Scripts/WorkshopCreation/Grid.cs
+++ Assets/General/Scripts/WorkshopCreation/Grid.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using Grid;
-public class GridCreation : MonoBehaviour
+public class Grid : MonoBehaviour
 {
-   public float width, height;
+   public float Width, Height;
    public Slider heightSlide;
    public Slider widthSlide;
     [SerializeField] Transform cam;
@@ -15,14 +14,14 @@ public class GridCreation : MonoBehaviour
     private Vector3 startPos;
     private float startZoom;
 
-   public void GenerateGrid()
+   public void RenderGrid()
    {
-        height = heightSlide.value;
-        width = widthSlide.value;
-        Debug.Log(height);
-        Debug.Log(width);
-        for(int x = 0; x < width; x++) { 
-            for(int y = 0; y<height; y++) { 
+        Height = heightSlide.value;
+        Width = widthSlide.value;
+        // Debug.Log(height);
+        // Debug.Log(width);
+        for(int x = 0; x < Width; x++) { 
+            for(int y = 0; y<Height; y++) { 
         
                 var spawnedTile = Instantiate(tilePrefab, new Vector3(x,y), Quaternion.identity);
                 spawnedTile.name = $"Tile ({x},{y})";
@@ -32,23 +31,24 @@ public class GridCreation : MonoBehaviour
         }
     }
     
-        float maximum = Mathf.Max(width, height);
+        float maximum = Mathf.Max(Width, Height);
     
         camera.orthographicSize = maximum/1.5f + maximum/100;
     
         float cameraHeight = 2.0f * camera.orthographicSize;
         float cameraWidth = cameraHeight * camera.aspect;
-        cam.transform.position = new Vector3(width/2.0f + cameraWidth/8.0f , height/1.85f - 0.5f, -10);
+        cam.transform.position = new Vector3(Width/2.0f + cameraWidth/8.0f , Height/1.85f - 0.5f, -10);
         camera.backgroundColor = new Color(0.75f, 0.75f, 0.75f, 1.0f);
 
         startPos = camera.transform.position;
         startZoom = camera.orthographicSize;
      
-        gameObject.GetComponent<TileManager>().occupied = new int[(int) width,(int) height];
+        gameObject.GetComponent<TileManager>().occupied = new int[(int) Width,(int) Height];
    }
 
    public void resetGrid() {
         camera.transform.position = startPos;
         camera.orthographicSize = startZoom;
    }
+
 }
